fad741a5ee01a6826cefa96a0521934f58b0ba35
suraphop <suraphop@mail.com>
2024-02-28 13:27:33 +0700

    update log

--- watch_dog_example.py
+++ watch_dog_example.py
@@ -1,9 +1,17 @@
 import time
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
+import logging
 
-def do_something():
-    print('do some thing')
+def do_something(path):
+    try:
+        print(f'do some thing in {path}')
+        print(1/0)
+        time.sleep(5)
+        print('done')
+    except Exception as e:
+        logging.error(f'file name : {path} , error: {e}')
+        
 
 class EventHandler(PatternMatchingEventHandler):
     def __init__(self):
@@ -35,14 +43,25 @@ class EventHandler(PatternMatchingEventHandler):
                 print("Directory modified:", event.src_path)
             else:
                 print("File modified:", event.src_path)
-                do_something()
-                       
-path = r'C:\\Users\\nmb_m\\OneDrive\\Desktop\\watchdog\\data'
-event_handler = EventHandler()
+                do_something(event.src_path)
+                
+logname = 'log'
+logging.basicConfig(filename=logname,
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO)
 
+path = r'C:\\Users\\nmb_m\\OneDrive\\Desktop\\watchdog\\data'
+
+event_handler = EventHandler()
 observer = Observer()
 observer.schedule(event_handler, path, recursive=True)
-observer.start()
+    
+try:
+    observer.start()
+except Exception as e:
+    logging.error(f'error: {e}')
 
 try:
     while True:
